d47e2cbc41fb4fd57166dafa76f9470f8c69e2f7
Nubtastic1 <72994069+Nubtastic1@users.noreply.github.com>
2021-03-19 00:00:38 -0400

    updated generator.js, work still needed on error handling

--- generator.js
+++ generator.js
@@ -5,8 +5,7 @@ const readlineSync = require('readline-sync'); //Readline Sync Module, allows to
 
 const requestLink = 'https://www.themealdb.com/api/json/v1/1/filter.php?i='
 var final;
-function getIng()
-{
+function getIng() {
     var ingredient = readlineSync.question('What is your main ingredient?');
     final = requestLink.concat(ingredient);
 }
@@ -18,14 +17,53 @@ async function getDataset() {
     return data;
 }
 
-async function loadRecipes()
-{
+async function loadRecipes() {
   const { meals } = await getDataset();
   const mealNames = meals.map(meal => meal.strMeal)
   console.log("You can make these meals");
   console.log(mealNames.join("\n"));
 }
 
+/*function errorHandler(error) {
+  console.log("This item is not found within our recipe list:");
+  console.log({ meals });
+}
+*/
+
 
 getIng();
-loadRecipes();
+//try {
+  loadRecipes()
+    .then({ meals } , { meals })
+    .catch({ meals } , console.error(error.message))
+    .finally(() => console.log("This item is not found within our recipe list:" + { meals }));
+/*} catch ({ meals }) {
+  errorHandler({ meals });
+}
+*/
+
+//not sure where the code set a Promise, but it's either that or error got thrown somewhere unexpected
+//assumed { meals } or mealNames would be cause of link fetching error, they should already be throwing an error object if error message is indicative
+//tried to handle error via .catch() and try/catch under loadRecipe() callback, did not work, perhaps misused or misplaced
+//tried to handle via callbacks in loadRecipe() when using callback getDataset() and rerouting to errorHandler()
+
+
+/*some default error messages displayed (for archiving purposes):
+(node:16440) UnhandledPromiseRejectionWarning: TypeError: Cannot read property 'map' of null
+    at loadRecipes (C:\Users\Nubtastic\Documents\Code Stuff\recipe-generator-master\generator.js:28:27)
+(Use `node --trace-warnings ...` to show where the warning was created)
+(node:16440) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside 
+of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the 
+node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
+(node:16440) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
+
+
+(node:10452) UnhandledPromiseRejectionWarning: TypeError: Cannot read property 'map' of null
+    at loadRecipes (C:\Users\Nubtastic\Documents\Code Stuff\recipe-generator-master\generator.js:28:27)
+    at processTicksAndRejections (internal/process/task_queues.js:93:5)
+(Use `node --trace-warnings ...` to show where the warning was created)
+(node:10452) UnhandledPromiseRejectionWarning: Unhandled promise rejection. This error originated either by throwing inside 
+of an async function without a catch block, or by rejecting a promise which was not handled with .catch(). To terminate the 
+node process on unhandled promise rejection, use the CLI flag `--unhandled-rejections=strict` (see https://nodejs.org/api/cli.html#cli_unhandled_rejections_mode). (rejection id: 1)
+(node:10452) [DEP0018] DeprecationWarning: Unhandled promise rejections are deprecated. In the future, promise rejections that are not handled will terminate the Node.js process with a non-zero exit code.
+*/
